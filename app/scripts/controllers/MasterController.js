816f99596e3f2a253ff1a73f8c800527f7697b18
Aqeel Bhat <aqeel.bhat@synechron.com>
2014-12-01 19:18:36 +0530

    Bind user data object to the view
    - Fetch jquery before oauth.js (Dependency)
    - Replace local scope with the Global scope
    - Added logout functionality
    - Bind user data with the photoramp template
    - Style photoramp.scss

--- app/scripts/controllers/MasterController.js
+++ app/scripts/controllers/MasterController.js
@@ -1,36 +1,43 @@
 /*jslint devel: true*/
 /*global Photoramp, OAuth*/
-Photoramp.controller("MasterController", function ($scope, $q, InstagramService) {
+Photoramp.controller("MasterController", function ($location, $rootScope, $q, InstagramService) {
     'use strict';
     console.info("Master Controller Loaded");
-    
-    $scope.self;
-    
+
+    //Full name
+    $rootScope.user;
+
     InstagramService.initialize();
-    
+
     //using the OAuth authorization result get user data
-    $scope.refreshTimeline = function() {
-        InstagramService.getSelf().then(function(data) {
-            $scope.self = data;
-            console.log($scope.self);
+    $rootScope.getSelfData = function () {
+        InstagramService.getSelf().then(function (response) {
+            if (response.meta.code = 200) {
+                $rootScope.user = response.data;
+                console.log($rootScope.user);
+            }
         });
-    };
-    
+    };    
     //when the user clicks the connect button, the popup authorization window opens
-    $scope.connectButton = function() {
-        InstagramService.connectInstagram().then(function() {
+    $rootScope.connect = function (event) {
+        event.preventDefault();
+        InstagramService.connectInstagram().then(function () {
             if (InstagramService.isReady()) {
-                //if the authorization is successful, then route to view
+                //when ready get data from instagram and route to the view
+                $rootScope.getSelfData();
+                $location.path('/photoramp');
             }
         });
     }
-    
-    $scope.signOut = function() {
+
+    //Clear data annd route to logout view
+    $rootScope.logOut = function () {
         InstagramService.clearCache();
+        $location.path('/logout');
     }
 
-    //if the user is a returning user, hide the sign in button and display the tweets
+    //if the user is a returning user, and route to photoramp
     if (InstagramService.isReady()) {
-        $scope.refreshTimeline();
+        $location.path('/photoramp');
     }
 });
