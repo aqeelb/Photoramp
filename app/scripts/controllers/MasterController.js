933460d4a3daf746f9bc97155dfd656a2bc58ed8
Aqeel Bhat <aqeel.bhat@synechron.com>
2014-12-01 20:43:21 +0530

    Get user media service implemented
    - Refactored Instagram Data Factory
    - Get user media service implemented

--- app/scripts/controllers/MasterController.js
+++ app/scripts/controllers/MasterController.js
@@ -4,20 +4,33 @@ Photoramp.controller("MasterController", function ($location, $rootScope, $q, In
     'use strict';
     console.info("Master Controller Loaded");
 
-    //Full name
+    //authenticated user
     $rootScope.user;
+    $rootScope.media;
+
 
     InstagramService.initialize();
 
     //using the OAuth authorization result get user data
     $rootScope.getSelfData = function () {
-        InstagramService.getSelf().then(function (response) {
-            if (response.meta.code = 200) {
+        InstagramService.getUser().then(function (response) {
+            if (response.meta.code === 200) {
                 $rootScope.user = response.data;
                 console.log($rootScope.user);
             }
         });
-    };    
+    };
+
+    //get self media
+    $rootScope.getSelfMedia = function () {
+        InstagramService.getMedia().then(function (response) {
+            if (response.meta.code === 200) {
+                $rootScope.media = response.data;
+                console.log(response);
+            }
+        });
+    };
+
     //when the user clicks the connect button, the popup authorization window opens
     $rootScope.connect = function (event) {
         event.preventDefault();
@@ -25,19 +38,22 @@ Photoramp.controller("MasterController", function ($location, $rootScope, $q, In
             if (InstagramService.isReady()) {
                 //when ready get data from instagram and route to the view
                 $rootScope.getSelfData();
+                $rootScope.getSelfMedia();
                 $location.path('/photoramp');
             }
         });
     }
 
-    //Clear data annd route to logout view
+    //Clear cache annd route to logout view
     $rootScope.logOut = function () {
         InstagramService.clearCache();
         $location.path('/logout');
     }
 
-    //if the user is a returning user, and route to photoramp
+    //if the user is a returning user route to photoramp
     if (InstagramService.isReady()) {
+        $rootScope.getSelfData();
+        $rootScope.getSelfMedia();
         $location.path('/photoramp');
     }
 });
